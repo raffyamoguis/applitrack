18806f76232014364c8b762513afb7a419754c98
unknown <raffy.amoguis.projects@gmail.com>
2023-07-11 21:11:50 +0800

    added drawer

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,27 +1,61 @@
 import React from "react";
-import { Flex, Spacer, Divider, HStack, Avatar, Text } from "@chakra-ui/react";
+import {
+  Flex,
+  Spacer,
+  Divider,
+  HStack,
+  Avatar,
+  Text,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  useMediaQuery,
+} from "@chakra-ui/react";
 
 // Components
 import HeaderLink from "./headerlink";
+import Drawer from "./drawer";
 
 const Header: React.FC = () => {
+  const [isSmallerDevice] = useMediaQuery("(max-width: 600px)");
+
   return (
     <>
-      <Flex minWidth="max-content" alignItems="center" gap="2" p={4}>
-        <Text fontSize="3xl" as="b">
+      <Flex
+        minWidth="max-content"
+        alignItems="center"
+        gap="2"
+        p={isSmallerDevice ? 2 : 4}
+      >
+        <Text fontSize={{ base: "xl", sm: "3xl" }} as="b">
           AppliTrak
         </Text>
 
         <Spacer />
-        <HStack spacing="20px">
-          <HeaderLink title="Overview" to="/" />
-          <HeaderLink title="Applications" to="applications" />
-          <Avatar
-            size="md"
-            name="Raffy Amoguis"
-            src="https://bit.ly/ryan-florence"
-          />
-        </HStack>
+        {isSmallerDevice ? (
+          <Drawer />
+        ) : (
+          <HStack spacing="20px">
+            <HeaderLink title="Overview" to="/" />
+            <HeaderLink title="Applications" to="applications" />
+
+            <Menu>
+              <MenuButton>
+                <Avatar
+                  size={`${isSmallerDevice ? "sm" : "md"}`}
+                  name="Raffy Amoguis"
+                  src="https://bit.ly/ryan-florence"
+                />
+              </MenuButton>
+              <MenuList>
+                <MenuItem>Darkmode</MenuItem>
+                <MenuItem>Profile</MenuItem>
+                <MenuItem>Logout</MenuItem>
+              </MenuList>
+            </Menu>
+          </HStack>
+        )}
       </Flex>
 
       <Divider />
---
+++ src/components/header/drawer.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import {
+  IconButton,
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  VStack,
+  StackDivider,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { IconMenuDeep } from "@tabler/icons-react";
+
+import HeaderLink from "./headerlink";
+
+const drawer: React.FC = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <IconButton
+        aria-label="Show drawer."
+        colorScheme="gray"
+        variant="ghost"
+        icon={<IconMenuDeep />}
+        onClick={onOpen}
+      />
+      <Drawer isOpen={isOpen} placement="right" size="xs" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Account</DrawerHeader>
+
+          <DrawerBody>
+            <VStack
+              divider={<StackDivider borderColor="gray.200" />}
+              spacing={3}
+              align="stretch"
+            >
+              <HeaderLink title="Overview" to="/" />
+              <HeaderLink title="Applications" to="/applications" />
+              <HeaderLink title="Profile" to="/profile" />
+              <HeaderLink title="Logout" to="/logout" />
+            </VStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default drawer;
