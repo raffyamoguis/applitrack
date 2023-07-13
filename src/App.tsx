2fcf9e5aea6335bfcfd956d11650daca549d2f17
unknown <raffy.amoguis.projects@gmail.com>
2023-07-13 11:20:34 +0800

    added new routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Applications from "./pages/Applications";
 import NoMatch from "./pages/NoMatch";
+import Login from "./pages/auth/Login";
+import Signup from "./pages/auth/Signup";
 
 // Components
 import Header from "./components/header/Header";
@@ -14,6 +16,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/applications" element={<Applications />} />
         <Route path="*" element={<NoMatch />} />
       </Routes>
