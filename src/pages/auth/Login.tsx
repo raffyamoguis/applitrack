9aa4f152ad91d05c1316e615e823e89fd6c1e8ec
unknown <raffy.amoguis.projects@gmail.com>
2023-07-13 16:41:07 +0800

    updated auth breakpoints

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -11,6 +11,7 @@ import {
   InputRightElement,
   Button,
   Link,
+  Flex,
 } from "@chakra-ui/react";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 
@@ -21,15 +22,16 @@ const Login: React.FC = () => {
   return (
     <Box position="relative" h="100vh">
       <AbsoluteCenter p="4" color="gray" axis="both">
-        <Text mb="2" fontSize="sm" align="center">
+        <Text mb="2" align="center" fontSize={{ base: "xs", sm: "sm" }}>
           Login to use this awesome application. 🤨
         </Text>
         <Card
-          boxSize="lg"
+          boxSize={{ base: "xs", sm: "md", md: "lg" }}
+          h={{ base: "sm", sm: "md" }}
           borderRadius="xl"
           variant="outline"
           shadow="sm"
-          p="4"
+          p={{ base: "2", sm: "4" }}
         >
           <CardBody>
             <Text as="b" fontSize="xl">
@@ -38,27 +40,42 @@ const Login: React.FC = () => {
 
             <Input
               placeholder="Enter email"
-              size="lg"
-              mt="20"
-              mb="5"
+              size={{ base: "md", md: "lg" }}
+              mt={{ base: "10", md: "20" }}
+              mb={{ base: "3", sm: "5" }}
               focusBorderColor="#2f2f31"
             />
-            <InputGroup size="lg">
+            <InputGroup
+              mb={{ base: "3", sm: "5" }}
+              size={{ base: "md", md: "lg" }}
+              alignItems="center"
+            >
               <Input
                 pr="4.5rem"
                 type={show ? "text" : "password"}
                 placeholder="Enter password"
                 focusBorderColor="#2f2f31"
               />
-              <InputRightElement width="4.5rem" onClick={handleClick}>
-                {show ? (
-                  <ViewOffIcon color="gray" />
-                ) : (
-                  <ViewIcon color="gray" />
-                )}
-              </InputRightElement>
+              <Flex alignItems="center">
+                <InputRightElement
+                  width="4.5rem"
+                  h="100%"
+                  onClick={handleClick}
+                >
+                  {show ? (
+                    <ViewOffIcon color="gray" />
+                  ) : (
+                    <ViewIcon color="gray" />
+                  )}
+                </InputRightElement>
+              </Flex>
             </InputGroup>
-            <Button colorScheme="nigga" mt="10" w="100%">
+            <Button
+              colorScheme="nigga"
+              w="100%"
+              mt={{ base: "4", sm: "10" }}
+              size={{ base: "md", md: "lg" }}
+            >
               Login
             </Button>
 
@@ -68,9 +85,10 @@ const Login: React.FC = () => {
               style={{ textDecoration: "none" }}
             >
               <Text
-                _hover={{ color: "gray" }}
-                mt="5"
-                fontSize="sm"
+                _hover={{ color: "black" }}
+                color="gray"
+                mt={{ base: "3", sm: "5" }}
+                fontSize={{ base: "xs", sm: "sm" }}
                 align="center"
               >
                 Dont have an account create one. 😌
--- src/pages/auth/Signup.tsx
+++ src/pages/auth/Signup.tsx
@@ -1,11 +1,107 @@
-import React from "react";
-import { Box, AbsoluteCenter } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Link as ReactLink } from "react-router-dom";
+import {
+  Box,
+  AbsoluteCenter,
+  Card,
+  CardBody,
+  Text,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Button,
+  Link,
+  Flex,
+} from "@chakra-ui/react";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 
 const Login: React.FC = () => {
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
   return (
-    <Box position="relative" h="100px">
-      <AbsoluteCenter bg="tomato" p="4" color="white" axis="both">
-        Hello
+    <Box position="relative" h="100vh">
+      <AbsoluteCenter p="4" color="gray" axis="both">
+        <Text mb="2" fontSize={{ base: "xs", sm: "sm" }} align="center">
+          Signup to use this awesome application. 🤨
+        </Text>
+        <Card
+          boxSize={{ base: "xs", sm: "md", md: "lg" }}
+          h={{ base: "sm", sm: "md" }}
+          borderRadius="xl"
+          variant="outline"
+          shadow="sm"
+          p={{ base: "2", sm: "4" }}
+        >
+          <CardBody>
+            <Text as="b" fontSize="xl">
+              Signup
+            </Text>
+
+            <Input
+              placeholder="Enter email"
+              size={{ base: "md", md: "lg" }}
+              mt={{ base: "6", sm: "10", md: "20" }}
+              mb={{ base: "3", sm: "5" }}
+              focusBorderColor="#2f2f31"
+            />
+            <InputGroup
+              mb={{ base: "3", sm: "5" }}
+              size={{ base: "md", md: "lg" }}
+              alignItems="center"
+            >
+              <Input
+                pr="4.5rem"
+                type={show ? "text" : "password"}
+                placeholder="Create password"
+                focusBorderColor="#2f2f31"
+              />
+              <Flex alignItems="center">
+                <InputRightElement
+                  width="4.5rem"
+                  h="100%"
+                  onClick={handleClick}
+                >
+                  {show ? (
+                    <ViewOffIcon color="gray" />
+                  ) : (
+                    <ViewIcon color="gray" />
+                  )}
+                </InputRightElement>
+              </Flex>
+            </InputGroup>
+            <InputGroup
+              mb={{ base: "3", sm: "5" }}
+              size={{ base: "md", md: "lg" }}
+            >
+              <Input
+                pr="4.5rem"
+                type={show ? "text" : "password"}
+                placeholder="Confirm password"
+                focusBorderColor="#2f2f31"
+              />
+            </InputGroup>
+            <Button
+              colorScheme="nigga"
+              mt={{ base: "2", sm: "10" }}
+              w="100%"
+              size={{ base: "md", md: "lg" }}
+            >
+              Create account
+            </Button>
+
+            <Link as={ReactLink} to="/login" style={{ textDecoration: "none" }}>
+              <Text
+                _hover={{ color: "black" }}
+                color="gray"
+                mt={{ base: "2", sm: "5" }}
+                fontSize={{ base: "xs", sm: "sm" }}
+                align="center"
+              >
+                Already have an account login. 🥱
+              </Text>
+            </Link>
+          </CardBody>
+        </Card>
       </AbsoluteCenter>
     </Box>
   );
