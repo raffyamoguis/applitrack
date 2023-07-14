b554074994f15cbb3add196c2e3cf10b96906ad3
unknown <raffy.amoguis.projects@gmail.com>
2023-07-14 16:17:41 +0800

    added fetchapplications as a hook

---
+++ src/hooks/useApplications.ts
@@ -0,0 +1,26 @@
+import { useQuery } from 'react-query';
+import {
+  COLLECTION_ID_APPLICATIONS,
+  DATABASE_ID,
+  databases,
+} from "../appwriteConfig";
+
+
+const fetchApplications = async () => {
+  try {
+    // Call the Appwrite API endpoint to fetch projects
+    const response = await databases.listDocuments(
+      DATABASE_ID,
+      COLLECTION_ID_APPLICATIONS
+    );
+    return response;
+  } catch (error) {
+    throw new Error('Failed to fetch projects');
+  }
+};
+
+const useApplications = () => {
+  return useQuery('applications', fetchApplications);
+};
+
+export default useApplications;
