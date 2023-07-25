ccb64fd2b8b707f2a598ef72043725cd5bde1fb1
unknown <raffy.amoguis.projects@gmail.com>
2023-07-25 22:49:54 +0800

    added search application hook

---
+++ src/hooks/application/useSearchApplication.ts
@@ -0,0 +1,32 @@
+import { useQuery } from 'react-query';
+import {Query} from "appwrite";
+import {
+  COLLECTION_ID_APPLICATIONS,
+  DATABASE_ID,
+  databases,
+} from "../../appwriteConfig";
+
+
+const searchApplications = async (query: string, user_id: string) => {
+    try {
+      // Call the Appwrite API endpoint to fetch projects
+      const response = await databases.listDocuments(
+        DATABASE_ID,
+        COLLECTION_ID_APPLICATIONS,
+        [
+            Query.equal("user_id", user_id),
+            Query.search('name', query),
+        ]
+      );
+      return response;
+    } catch (error) {
+      throw new Error('Failed to fetch applications');
+    }
+  
+};
+
+const useSearchApplication = (query: string, user_id:string) => {
+  return useQuery(['applications_search', query],() => searchApplications(query, user_id));
+};
+
+export default useSearchApplication;
