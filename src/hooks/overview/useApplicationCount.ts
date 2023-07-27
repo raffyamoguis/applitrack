837d98e2151ad8244ae907219005e9f686af52ee
unknown <raffy.amoguis.projects@gmail.com>
2023-07-27 09:53:18 +0800

    added new hook

---
+++ src/hooks/overview/useApplicationCount.ts
@@ -0,0 +1,28 @@
+import { useQuery } from 'react-query';
+import {
+  COLLECTION_ID_APPLICATIONS,
+  DATABASE_ID,
+  databases,
+} from "../../appwriteConfig";
+
+
+const fetchApplicationCount = async (queries?: any) => {
+    try {
+      // Call the Appwrite API endpoint to fetch projects
+      const response = await databases.listDocuments(
+        DATABASE_ID,
+        COLLECTION_ID_APPLICATIONS,
+        queries,
+      );
+      return response.total;
+    } catch (error) {
+      throw new Error('Failed to fetch applications');
+    }
+  
+};
+
+const useApplicationCount = (queries: any) => {
+  return useQuery('applicationCount',() => fetchApplicationCount(queries));
+};
+
+export default useApplicationCount;
