e023c975ed10b06d1edc02c261349feca052ddcb
Raffy Amoguis <raffy.amoguis1@gmail.com>
2023-09-28 10:46:03 +0800

    added new lib data

---
+++ src/lib/application/index.tsx
@@ -0,0 +1,11 @@
+export const filters = [
+  "No filter",
+  "Assesment interview",
+  "Initial interview",
+  "Technical interview",
+  "Final interview",
+  "Not selected",
+  "No update",
+];
+
+export const sortTypes = ["Ascending", "Descending"];
